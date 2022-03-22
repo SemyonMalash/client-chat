82db005c980e2214a10374c156f21f3bc4ad5455
SemyonMalash <floan732@gmail.com>
2022-03-23 01:39:24 +0300

    Practice

--- src/main/java/ru/itsjava/Main.java
+++ src/main/java/ru/itsjava/Main.java
@@ -1,24 +1,11 @@
 package ru.itsjava;
 
-import java.io.*;
-import java.net.Socket;
+import ru.itsjava.services.ClientServiceImpl;
 
 public class Main {
-    public static final int PORT = 8081;
-    public static final String HOST = "localhost";
 
-    public static void main(String[] args) throws IOException {
-        Socket socket = new Socket(HOST, PORT);
-
-        if (socket.isConnected()) {
-            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
-            serverWriter.println("Yo");
-            serverWriter.flush();
-
-            BufferedReader serverReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            while (true) {
-                System.out.println(serverReader.readLine());
-            }
-        }
+    public static void main(String[] args) {
+        ClientServiceImpl clientService = new ClientServiceImpl();
+        clientService.start();
     }
 }
