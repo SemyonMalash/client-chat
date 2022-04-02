98e04f636bab895145fd47c6a61b50ae7b68a7cf
SemyonMalash <floan732@gmail.com>
2022-04-02 23:55:49 +0300

    add ReauthorizationService

--- src/main/java/ru/itsjava/services/impl/AuthorizationServiceImpl.java
+++ src/main/java/ru/itsjava/services/impl/AuthorizationServiceImpl.java
@@ -3,6 +3,7 @@ package ru.itsjava.services.impl;
 import lombok.SneakyThrows;
 import ru.itsjava.services.AuthorizationService;
 import ru.itsjava.services.MessageInputService;
+import ru.itsjava.services.ReauthorizationService;
 
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -10,25 +11,28 @@ import java.net.Socket;
 import static ru.itsjava.services.impl.ClientServiceImpl.HOST;
 import static ru.itsjava.services.impl.ClientServiceImpl.PORT;
 
-public class AuthorizationServiceImpl implements AuthorizationService {
+public class AuthorizationServiceImpl implements AuthorizationService, ReauthorizationService {
+    String login;
+    String password;
+    Socket socket;
 
     @SneakyThrows
     @Override
     public void authorize() {
-        Socket socket = new Socket(HOST, PORT);
+        socket = new Socket(HOST, PORT);
+
         if (socket.isConnected()) {
-            new Thread(new SocketRunnable(socket)).start();
+            new Thread(new SocketRunnable(socket));
 
             PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
             MessageInputService messageInputService = new MessageInputServiceImpl(System.in);
 
             System.out.println("Введите свой логин:");
-            String login = messageInputService.getMessage();
+            login = messageInputService.getMessage();
 
             System.out.println("Введите свой пароль:");
-            String password = messageInputService.getMessage();
+            password = messageInputService.getMessage();
 
-            //!autho!login:password
             serverWriter.println("!autho!" + login + ":" + password);
             serverWriter.flush();
 
@@ -40,9 +44,36 @@ public class AuthorizationServiceImpl implements AuthorizationService {
                     socket.close();
                     System.exit(-1);
                 }
+                if (consoleMessage.equals("Login")) {
+                    reauthorize();
+                }
                 serverWriter.println(consoleMessage);
                 serverWriter.flush();
             }
+        }
+    }
+
+    @SneakyThrows
+    @Override
+    public void reauthorize() {
+        socket = new Socket(HOST, PORT);
+
+        if (socket.isConnected()) {
+            new Thread(new SocketRunnable(socket));
+
+            PrintWriter serverWriter = new PrintWriter(socket.getOutputStream());
+            MessageInputService messageInputService = new MessageInputServiceImpl(System.in);
+
+            System.out.println("Введите свой логин:");
+            login = messageInputService.getMessage();
+
+            System.out.println("Введите свой пароль:");
+            password = messageInputService.getMessage();
+
+            serverWriter.println("!reautho!" + login + ":" + password);
+            serverWriter.flush();
+
+            System.out.println("Вы успешно авторизованы");
         }
     }
 }
